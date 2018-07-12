c11819b8dbc84d7137d8d224dc2b1093fed164ff
xuanhuyit <32665662+xuanhuyit@users.noreply.github.com>
2018-07-12 15:25:17 +0700

    Add files via upload

--- js/renderColumns.js
+++ js/renderColumns.js
@@ -1,1 +1,1 @@
-module.exports=__NEXT_REGISTER_PAGE("/renderColumns",function(){return{page:webpackJsonp([3],{430:function(e,n,o){e.exports=o(183)}},[430]).default}});
+module.exports=__NEXT_REGISTER_PAGE("/renderColumns",function(){return{page:webpackJsonp([3],{433:function(e,n,o){e.exports=o(183)}},[433]).default}});
